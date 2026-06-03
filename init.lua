d802340e5972bc09fdae6e40ebd60ac3249494b1
Dan Ragnar <dan.ragnar@danragnar.se>
2026-06-03 07:55:43 +0200

    Change theme

--- init.lua
+++ init.lua
@@ -829,7 +829,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
@@ -974,6 +974,8 @@ require('lazy').setup({
     },
   },
 })
+vim.opt.background = 'dark'
+vim.cmd.colorscheme 'rose-pine'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,3 +1,10 @@
 return {
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  -- { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  -- 'nyoom-engineering/oxocarbon.nvim',
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  config = function()
+    vim.cmd 'colorscheme rose-pine'
+  end,
+  -- 'rebelot/kanagawa.nvim',
 }
